a3389fc621738a859b1151e9aefda1f68c7f47a0
sterk55 <daniel49571@gmail.com>
2022-08-09 21:00:26 -0500

    Taller23

--- src/router/router.js
+++ src/router/router.js
@@ -1,18 +1,41 @@
-import {createRouter, createWebHashHistory} from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import AboutPage from "@/modulos/pokemon/pages/AboutPage.vue"
 import ListPage from "../modulos/pokemon/pages/ListPage.vue"
 import PokemonPage from "../modulos/pokemon/pages/PokemonPage.vue"
 import NoPageFound from "../modulos/pokemon/pages/NoPageFound.vue"
 
-const routes=[ 
+/*const routes=[ 
     {path:'/', component: AboutPage},
     {path:'/list', component: ListPage},
     {path:'/id', component: PokemonPage},
     {path:'/:pathMatch(.*)*', component: NoPageFound},
 
 
+]*/
+
+const routes = [
+    {
+        path: '/',
+        component: () =>
+            import(/*webpackChunkName: "AboutPage"*/'../modulos/pokemon/pages/AboutPage')
+    },
+    {
+        path: '/list',
+        component: () =>
+            import(/*webpackChunkName: "ListPage"*/ '../modulos/pokemon/pages/ListPage')
+    },
+    {
+        path: '/pokemon',
+        component: () =>
+            import(/*webpackChunkName: "PokemonPage"*/ '../modulos/pokemon/pages/PokemonPage')
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        component: () => import(/*webpackChunkName: "NoPageFound"*/ '../modulos/pokemon/pages/NoPageFound')
+    }
 ]
+
 
 const router = createRouter({
     // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
